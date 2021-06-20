100ad020bdacfd448c307f5d68e79a6074e9bd89
karan235 <83571787+karan235@users.noreply.github.com>
2021-06-20 12:40:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var first_name ="Manvitha";
+var last_name ="Karanam";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var a =10;
+var b =5;
+var add =a+b;
+console.log(add);
+
+var substract =a-b;
+console.log(substract);
+
+var multiply =a*b;
+console.log(multiply);
+
+var divide =a/b;
+console.log(divide);
+
